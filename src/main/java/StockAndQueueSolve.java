a79bccc22f225feab05a3107b4942bd9e1a71aba
davidlee35@qq.com <1104229497@qq.com>
2022-02-12 17:07:47 +0800

    [155]最小栈

--- src/main/java/StockAndQueueSolve.java
+++ src/main/java/StockAndQueueSolve.java
@@ -1,3 +1,26 @@
-public class StockAndQueueSolve {
+import java.util.*;
 
+public class StockAndQueueSolve {
+    public static boolean isValid(String s) {
+        Stack<Character> left = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (int i = 0; i < s.length(); i++) {
+            if (Arrays.asList('(', '[', '{').contains(s.charAt(i))) {
+                left.push(s.charAt(i));
+            } else {
+                if (left.isEmpty()||map.get(left.pop())!=s.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return left.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "([)]";
+        System.out.println(isValid(s));
+    }
 }
