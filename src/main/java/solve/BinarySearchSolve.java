072c54e17fc897b994acd873d7cea0b6d4c364b9
davidlee35@qq.com <1104229497@qq.com>
2022-02-14 16:16:51 +0800

    [744]寻找比目标字母大的最小字母

--- src/main/java/solve/BinarySearchSolve.java
+++ src/main/java/solve/BinarySearchSolve.java
@@ -18,7 +18,21 @@ public class BinarySearchSolve {
         return low - 1;
     }
 
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length;
+        while (left<right) {
+            int middle = left + (right - left)/2;
+            if (letters[middle]> target) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return letters[left % letters.length];
+    }
+
     public static void main(String[] args) {
-        System.out.println(mySqrt(0));
+        char[] letters = new char[]{'c', 'f', 'j'};
+        System.out.println(nextGreatestLetter(letters,'j'));
     }
 }
